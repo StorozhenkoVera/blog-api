c347140602ae7e6a59400b6d3539a2a333077d16
Storozhenko_Vera <verun.mak@gmail.com>
2021-03-26 08:20:14 +0200

    blog

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  post '/login',                    to: 'sessions#create'
+  post '/logout',                   to: 'sessions#destroy'
+  get '/logged_in',                 to: 'sessions#is_logged_in?'
+  get '/signup',                    to: 'users#new'
+
+  post '/create_article',           to: 'articles#create'
+  get '/articles',                  to: 'articles#index'
+  get '/article/:id',               to: 'articles#show'
+
+  post '/create_comment',           to: 'comments#create'
+  get '/comment/:article_id',       to: 'comments#show'
+  get '/delete/:article_id',        to: 'comments#delete'
+
+  resources :users, only: [:create, :show, :index]
+
+end
